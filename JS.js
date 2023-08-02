4c875e9a69b20b39f72a9889cd42fc8c11c86006
faize700 <114169171+faize700@users.noreply.github.com>
2023-08-02 19:41:43 +0530

    Update JS.js

--- JS.js
+++ JS.js
@@ -5,7 +5,6 @@ btn.addEventListener("click", ()=>{
     const red = Math.trunc(Math.random() * 256);
     const green = Math.trunc(Math.random() * 256);
     const blue = Math.trunc(Math.random() * 256);
-    console.log(red,green,blue);
     body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
     currentColor.textContent = `${red}, ${green}, ${blue}`;
 });
